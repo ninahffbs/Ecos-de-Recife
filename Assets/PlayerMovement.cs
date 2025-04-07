cd64999dad786d53b6025a4ad2000e98b9ee593b
ninahffbs <nina.hffs@gmail.com>
2025-04-06 21:28:25 -0300

    Adicao de sprites e movimentacao do player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    private Vector2 movement;
+
+    void Update()
+{
+    // Entrada do jogador
+    movement.x = Input.GetAxisRaw("Horizontal");
+    movement.y = Input.GetAxisRaw("Vertical");
+
+    // Corrige pequenos valores para 0
+    float speed = movement.sqrMagnitude;
+    if (speed < 0.01f)
+        speed = 0f;
+
+    // Parâmetros de animação
+    animator.SetFloat("Speed", speed);
+
+    // Flip do personagem
+    if (movement.x > 0)
+        transform.localScale = new Vector3(1, 1, 1); // direita
+    else if (movement.x < 0)
+        transform.localScale = new Vector3(-1, 1, 1); // esquerda
+}
+
+
+    void FixedUpdate()
+    {
+        // Movimento do personagem
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
