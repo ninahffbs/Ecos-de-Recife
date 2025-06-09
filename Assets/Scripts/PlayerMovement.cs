1954f098f57b88eb28892b9be8ca864848c2a6fb
ninahffbs <nina.hffs@gmail.com>
2025-06-09 01:14:24 -0300

    adicionando logica de corrida, ataque e resolvendo bug de tamanho

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,39 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float walkSpeed = 5f; 
+    public float sprintSpeed = 8f; 
     public Rigidbody2D rb;
     public Animator animator;
     private Vector2 movement;
+    private float currentMoveSpeed;
+
+    void Start()
+    {
+        currentMoveSpeed = walkSpeed;
+    }
 
     void Update()
-{
-    // Entrada do jogador
-    movement.x = Input.GetAxisRaw("Horizontal");
-    movement.y = Input.GetAxisRaw("Vertical");
-    
-    if(Input.GetKeyDown(KeyCode.Space)){
-        animator.SetTrigger("Attack");
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize(); 
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentMoveSpeed = sprintSpeed;
+            animator.SetFloat("AnimationSpeedMultiplier", 1.5f); // Example: 1.5x normal speed
+        }
+        
+        else
+        {
+            currentMoveSpeed = walkSpeed;
+            animator.SetFloat("AnimationSpeedMultiplier", 1.0f);
+        }
+
+        float speed = movement.sqrMagnitude; 
+        if (speed < 0.01f)
+            speed = 0f;
+
+        animator.SetFloat("Speed", speed); 
+
+        if (movement.x > 0)
+            transform.localScale = new Vector3(3, 3, 3); 
+        else if (movement.x < 0)
+            transform.localScale = new Vector3(-3, 3, 3);
     }
-    // Corrige pequenos valores para 0
-    float speed = movement.sqrMagnitude;
-    if (speed < 0.01f)
-        speed = 0f;
-
-    // Parâmetros de animação
-    animator.SetFloat("Speed", speed);
-
-    // Flip do personagem
-    if (movement.x > 0)
-        transform.localScale = new Vector3(3, 3, 3); // direita
-    else if (movement.x < 0)
-        transform.localScale = new Vector3(-3, 3, 3); // esquerda
-}
-
 
     void FixedUpdate()
     {
-        // Movimento do personagem
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * currentMoveSpeed * Time.fixedDeltaTime);
     }
 }
